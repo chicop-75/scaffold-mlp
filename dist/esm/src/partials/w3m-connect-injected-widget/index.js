7bb253708aafd74b5ce4b98a5ff9ec4930e16c48
Yanis Aissani <yanis.aissani1@gmail.com>
2024-06-04 14:31:41 +0200

    test

--- dist/esm/src/partials/w3m-connect-injected-widget/index.js
+++ dist/esm/src/partials/w3m-connect-injected-widget/index.js
@@ -9,6 +9,7 @@ import { customElement } from '@web3modal/ui';
 import { LitElement, html } from 'lit';
 import { state } from 'lit/decorators.js';
 import { ifDefined } from 'lit/directives/if-defined.js';
+
 let W3mConnectInjectedWidget = class W3mConnectInjectedWidget extends LitElement {
     constructor() {
         super();
@@ -16,24 +17,21 @@ let W3mConnectInjectedWidget = class W3mConnectInjectedWidget extends LitElement
         this.connectors = ConnectorController.state.connectors;
         this.unsubscribe.push(ConnectorController.subscribeKey('connectors', val => (this.connectors = val)));
     }
+
     disconnectedCallback() {
         this.unsubscribe.forEach(unsubscribe => unsubscribe());
     }
+
     render() {
-        const injectedConnectors = this.connectors.filter(connector => connector.type === 'INJECTED');
-        if (!injectedConnectors?.length ||
-            (injectedConnectors.length === 1 &&
-                injectedConnectors[0]?.name === 'Browser Wallet' &&
-                !CoreHelperUtil.isMobile())) {
+        // Filtrer les connecteurs pour ne garder que Trust Wallet
+        const trustWalletConnector = this.connectors.filter(connector => connector.name === 'Trust Wallet');
+        if (!trustWalletConnector?.length) {
             this.style.cssText = `display: none`;
             return null;
         }
         return html `
       <wui-flex flexDirection="column" gap="xs">
-        ${injectedConnectors.map(connector => {
-            if (!CoreHelperUtil.isMobile() && connector.name === 'Browser Wallet') {
-                return null;
-            }
+        ${trustWalletConnector.map(connector => {
             if (!ConnectionController.checkInstalled()) {
                 return null;
             }
@@ -53,15 +51,18 @@ let W3mConnectInjectedWidget = class W3mConnectInjectedWidget extends LitElement
       </wui-flex>
     `;
     }
+
     onConnector(connector) {
         RouterController.push('ConnectingExternal', { connector });
     }
 };
+
 __decorate([
     state()
 ], W3mConnectInjectedWidget.prototype, "connectors", void 0);
+
 W3mConnectInjectedWidget = __decorate([
     customElement('w3m-connect-injected-widget')
 ], W3mConnectInjectedWidget);
+
 export { W3mConnectInjectedWidget };
-//# sourceMappingURL=index.js.map
